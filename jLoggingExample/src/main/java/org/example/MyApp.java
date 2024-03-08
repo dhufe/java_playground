42cde4dfc593294e5282a4726092d39b0be4521a
Daniel Hufschläger <daniel.hufschlaeger@blha.brandenburg.de>
2024-03-08 10:08:49 +0100

    Log4J is working.

--- jLoggingExample/src/main/java/org/example/MyApp.java
+++ jLoggingExample/src/main/java/org/example/MyApp.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MyApp {
-    private static final Logger logger = Logger.getLogger( MyApp.class.getName() );
+    private static final Logger logger = LogManager.getLogger( MyApp.class );
 
     public static void main(String[] args) {
         logger.trace("We've just greeted the user!");
