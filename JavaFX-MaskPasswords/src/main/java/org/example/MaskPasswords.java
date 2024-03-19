2f037a34df6d8135ebc900694093ff2ba0cdf19b
Daniel Hufschläger <daniel@hufschlaeger.net>
2024-03-19 17:27:03 +0100

    Adding DefaultTextConverter.

--- JavaFX-MaskPasswords/src/main/java/org/example/MaskPasswords.java
+++ JavaFX-MaskPasswords/src/main/java/org/example/MaskPasswords.java
@@ -11,16 +11,14 @@ import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener.Change;
-import javafx.beans.property.ReadOnlyObjectWrapper;
 
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javafx.util.converter.DefaultStringConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -30,9 +28,10 @@ public class MaskPasswords extends Application {
     private boolean bState = false;
 
     @Override
-    public void start (Stage primaryStage) {
+    public void start(Stage primaryStage) {
         TableView<ConfigModel> configTable = new TableView<>();
         ToggleButton extBtn = new ToggleButton();
+        CheckBox chkBox = new CheckBox("Toggle unmask");
         // standard column stuff:
         TableColumn<ConfigModel, String> KeyCol = new TableColumn<>("Key");
         KeyCol.setCellValueFactory(cellData -> cellData.getValue().keyProperty());
@@ -47,12 +46,21 @@ public class MaskPasswords extends Application {
         // real or masked password, and may
         // need to update if configWithShownPasswords changes:
         ValueCol.setCellFactory(c -> {
+            DefaultStringConverter converter = new DefaultStringConverter();
             // textfield cell for editing values:
-            TableCell cell = new TableCell<ConfigModel, String>();
+            TextFieldTableCell cell = new TextFieldTableCell<ConfigModel, String>(converter);
             // if the cell is reused for an item from a different row, update it:
-            cell.indexProperty().addListener((obs, oldIndex, newIndex) -> updateCell(configWithShownPasswords, cell));
+            cell.indexProperty().addListener((obs, oldIndex, newIndex) -> {
+                updateCell(configWithShownPasswords, cell);
+            });
             // if the password changes, update:
-            cell.itemProperty().addListener((obs, oldItem, newItem) -> updateCell(configWithShownPasswords, cell));
+            cell.itemProperty().addListener((obs, oldItem, newItem) -> {
+                updateCell(configWithShownPasswords, cell);
+            });
+
+            cell.textProperty().addListener((obs, oldItem, newItem) -> {
+                updateCell(configWithShownPasswords, cell);
+            });
             // if the set of users with shown password changes, update the cell:
             configWithShownPasswords.addListener((Change<? extends ConfigModel> change) -> updateCell(configWithShownPasswords, cell));
             return cell;
@@ -74,8 +82,26 @@ public class MaskPasswords extends Application {
                 configWithShownPasswords.addAll(configTable.getItems());
                 bState = true;
             } else {
-                configWithShownPasswords.removeAll(configTable.getItems());
+                configTable.getItems().forEach(configWithShownPasswords::remove);
                 bState = false;
+            }
+        });
+
+        extBtn.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            chkBox.setSelected( ! obs.getValue() );
+        });
+
+        chkBox.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            extBtn.setSelected ( ! obs.getValue() );
+        });
+
+        chkBox.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (!bState) {
+                configWithShownPasswords.addAll(configTable.getItems());
+                bState = false;
+            } else {
+                configTable.getItems().forEach(configWithShownPasswords::remove);
+                bState = true;
             }
         });
 
@@ -85,28 +111,29 @@ public class MaskPasswords extends Application {
         VBox pane = new VBox();
         pane.getChildren().add(configTable);
         pane.getChildren().add(extBtn);
+        pane.getChildren().add(chkBox);
 
         Scene scene = new Scene(pane, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private String maskPassword (String text) {
+    private String maskPassword(String text) {
         char[] chars = new char[text.length()];
         Arrays.fill(chars, '*');
         return new String(chars);
     }
 
-    private List<ConfigModel> createData () {
-        return IntStream.rangeClosed(1, 10).mapToObj(i -> {
+    private List<ConfigModel> createData() {
+        return IntStream.rangeClosed(1, 50).mapToObj(i -> {
             ConfigModel model;
-            if (i % 2 == 0) model = new ConfigModel("User " + i, randomPassword());
+            if (i % 5 == 0) model = new ConfigModel("User " + i, randomPassword());
             else model = new ConfigModel("UserPass" + i, randomPassword());
             return model;
         }).collect(Collectors.toList());
     }
 
-    private String randomPassword () {
+    private String randomPassword() {
         int pwSize = 6 + RNG.nextInt(5);
         char[] chars = new char[pwSize];
         for (int i = 0; i < chars.length; i++) {
@@ -116,7 +143,7 @@ public class MaskPasswords extends Application {
     }
 
 
-    private void updateCell (ObservableSet<ConfigModel> usersWithShownPasswords, TableCell<ConfigModel, String> cell) {
+    private void updateCell(ObservableSet<ConfigModel> usersWithShownPasswords, TextFieldTableCell<ConfigModel, String> cell) {
         int index = cell.getIndex();
         TableView<ConfigModel> table = cell.getTableView();
         if (index < 0 || index >= table.getItems().size()) {
@@ -134,7 +161,7 @@ public class MaskPasswords extends Application {
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 }
