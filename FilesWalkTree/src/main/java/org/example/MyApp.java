a9c60c96819b342ceed4ab6dc3ec18e662d08cca
Daniel Hufschläger <daniel@hufschlaeger.net>
2025-03-28 14:35:21 +0100

    Creating a config-File now. But there is some hassle with multi-depth folders.

--- FilesWalkTree/src/main/java/org/example/MyApp.java
+++ FilesWalkTree/src/main/java/org/example/MyApp.java
@@ -1,15 +1,26 @@
 package org.example;
 
+import de.dit.api.dimag.controlfile.ControlfileController;
+import de.dit.api.dimag.controlfile.extended.types.VerzeichnungseinheitExtended;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MyApp {
 
+    private static final Logger logger = LogManager.getLogger( MyApp.class.getSimpleName() );
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
-        Path startingDir = Path.of(System.getProperty("user.dir"));
+        Path startingDir = Path.of(System.getProperty("user.dir") , "testdata");
+        logger.info("Starting directory: {}", startingDir);
         PrintFiles pf = new PrintFiles();
         Files.walkFileTree(startingDir, pf);
+
+        VerzeichnungseinheitExtended obj = pf.getVerz();
+        ControlfileController ctrl = new ControlfileController();
+        System.out.println(ctrl.getAsXml(obj));
     }
 }
--- FilesWalkTree/src/main/java/org/example/PrintFiles.java
+++ FilesWalkTree/src/main/java/org/example/PrintFiles.java
@@ -5,42 +5,72 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Arrays;
 
-public class PrintFiles
-        extends SimpleFileVisitor<Path> {
+import de.dit.api.dimag.controlfile.VerzObjExtended;
+import de.dit.api.dimag.controlfile.Verzeichnungseinheit;
+import de.dit.api.dimag.controlfile.extended.types.VerzeichnungseinheitExtended;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PrintFiles extends SimpleFileVisitor<Path> {
+
+    private static final Logger logger = LogManager.getLogger(PrintFiles.class.getSimpleName());
+
+    public VerzeichnungseinheitExtended getVerz() {
+        return verz;
+    }
+
+    private VerzeichnungseinheitExtended verz = new VerzeichnungseinheitExtended();
+    private VerzObjExtended child = null;
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-        if (attrs.isSymbolicLink()) {
-            System.out.format("Symbolic link: %s ", file);
+        if (file.getFileName().toString().equals(".DS_Store")) {
+            logger.debug("Ignoring file: {} ", file);
+        } else if (attrs.isSymbolicLink()) {
+            logger.debug("Symbolic link: {} ", file);
         } else if (attrs.isRegularFile()) {
-            System.out.format("Regular file: %s ", file);
+            VerzObjExtended obj = new VerzObjExtended();
+            obj.setFile(file);
+            obj.setFilepath(file.toString());
+            obj.setSftpDateiname(file.getFileName().toString());
+            if (this.child != null) {
+                child.getVerzObj().add(obj);
+            } else {
+                verz.getVerzObj().add(obj);
+            }
+            logger.debug("Regular file: {} ", file);
         } else {
-            System.out.format("Other: %s ", file);
+            logger.debug("Unrecognized file: {} ", file);
         }
-        System.out.println("(" + attrs.size() + "bytes)");
 
+        logger.debug("{} {} bytes", file.toAbsolutePath(), attrs.size());
         return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-        System.out.format("Directory entered: %s%n", dir);
+        if ( child == null ) {
+            child = new VerzObjExtended();
+        }
+        logger.debug("Pre visit directory: {} ", dir);
         return FileVisitResult.CONTINUE;
     }
 
     @Override
-    public FileVisitResult postVisitDirectory(Path dir,
-                                              IOException exc) {
-        System.out.format("Directory leaved: %s%n", dir);
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        if ( child != null ) {
+            verz.getVerzObj().add(this.child);
+            this.child = null;
+        }
+
+        logger.debug("Post visit directory: {} ", dir);
         return FileVisitResult.CONTINUE;
     }
 
     @Override
-    public FileVisitResult visitFileFailed(Path file,
-                                           IOException exc) {
-        System.err.println(Arrays.toString(exc.getStackTrace()));
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
+        logger.error(exc.getMessage());
         return FileVisitResult.CONTINUE;
     }
 }
