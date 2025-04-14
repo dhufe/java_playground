a31363e6278366cc8e17f47d3da5fe770b43cd31
Daniel Hufschläger <Daniel.Hufschlaeger@blha.brandenburg.de>
2025-04-14 12:57:13 +0200

    Adding Spring boot security and jwt demo.

---
+++ jwtdemo/src/test/java/com/dhufe/jwtdemo/JwtdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhufe.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
